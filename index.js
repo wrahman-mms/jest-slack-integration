4ce742ab2e784e67a19148fa16b02885347d189d
Wajahat Rahman <wrahman@mms.org>
2023-04-11 14:47:19 +0500

    Updated text in results notification

--- index.js
+++ index.js
@@ -1,84 +1,86 @@
-const readPkg = require('read-pkg')
-const request = require('request')
-const bugsnag = require('bugsnag')
+const readPkg = require("read-pkg");
+const request = require("request");
+const bugsnag = require("bugsnag");
 
 /**
  * function to send slack your test notifications
  * @param {*} testResults this is the parameter passed through by Jest containing the test results
  */
-module.exports = testResults => {
+module.exports = (testResults) => {
   try {
-    const packageJson = readPkg.sync(process.cwd())
+    const packageJson = readPkg.sync(process.cwd());
 
-    bugsnag.register(packageJson.jestSlackIntegration.bugsnagAPI)
+    bugsnag.register(packageJson.jestSlackIntegration.bugsnagAPI);
 
-    const failedMessage = `<!here> Bad News! *${testResults.numFailedTestSuites}* test suites have failed and *${testResults.numFailedTests}* tests have failed :( *${testResults.numPassedTests}* tests have passed. Please have a look at your tests.`
-    const passedMessage = `<!here> Great News! All *${testResults.numPassedTests}* tests have passed! Code coverage is coming soon..`
-    const failedTests = testResults.numFailedTestSuites > 0 || testResults.numFailedTests > 0
-    const text = failedTests ? failedMessage : passedMessage
-    const borderColor = failedTests ? '#a30200' : '#36a64f'
+    const failedMessage = `<!here> Bad News! *${testResults.numFailedTestSuites}* test suites have failed and *${testResults.numFailedTests}* tests have failed :( *${testResults.numPassedTests}* tests have passed. Please have a look at your tests.`;
+    const passedMessage = `<!here> Great News! All *${testResults.numPassedTests}* tests have passed! Code coverage is coming soon..`;
+    const failedTests =
+      testResults.numFailedTestSuites > 0 || testResults.numFailedTests > 0;
+    const text = failedTests ? failedMessage : passedMessage;
+    const borderColor = failedTests ? "#a30200" : "#36a64f";
 
     const slackMessage = {
       text: text,
       attachments: [
         {
-          fallback: 'Required plain-text summary of the attachment.',
+          fallback: "Required plain-text summary of the attachment.",
           color: borderColor,
-          title: 'Your Test Results Are In!',
-          author_name: (packageJson.jestSlackIntegration.appName) ? packageJson.jestSlackIntegration.appName : 'App',
-          mrkdwn_in: ['text'],
-          text: 'Here is a quick summary of the jest test results..   \nrun `jest` locally to see all the details \n..',
+          title: "",
+          author_name: packageJson.jestSlackIntegration.appName
+            ? packageJson.jestSlackIntegration.appName
+            : "App",
+          mrkdwn_in: ["text"],
+          text: "",
           fields: [
-              {
-                title: 'Total Tests Suites',
-                value: testResults.numTotalTestSuites,
-                short: false
-              },
-              {
-                title: 'Total Tests',
-                value: testResults.numTotalTests,
-                short: false
-              },
-              {
-                  title: 'Test Suites Passing',
-                  value: testResults.numPassedTestSuites,
-                  short: false
-              },
-              {
-                  title: 'Test Suites Failing',
-                  value: testResults.numFailedTestSuites,
-                  short: false
-              },
-              {
-                  title: 'Total Tests Passing',
-                  value: testResults.numPassedTests,
-                  short: false
-              },
-              {
-                  title: 'Total Tests Failing',
-                  value: testResults.numFailedTests,
-                  short: false
-              }
+            {
+              title: "Total Tests Suites",
+              value: testResults.numTotalTestSuites,
+              short: false,
+            },
+            {
+              title: "Total Tests",
+              value: testResults.numTotalTests,
+              short: false,
+            },
+            {
+              title: "Test Suites Passing",
+              value: testResults.numPassedTestSuites,
+              short: false,
+            },
+            {
+              title: "Test Suites Failing",
+              value: testResults.numFailedTestSuites,
+              short: false,
+            },
+            {
+              title: "Total Tests Passing",
+              value: testResults.numPassedTests,
+              short: false,
+            },
+            {
+              title: "Total Tests Failing",
+              value: testResults.numFailedTests,
+              short: false,
+            },
           ],
-          'footer': 'jest-slack-integration by Jameel Mukadam'
-        }
-      ]
-    }
+        },
+      ],
+    };
 
     const options = {
       uri: packageJson.jestSlackIntegration.webhookUrl,
-      method: 'POST',
+      method: "POST",
       json: slackMessage,
-      mrkdwn: true
-    }
+      mrkdwn: true,
+    };
 
     try {
-      request(options)
+      request(options);
     } catch (err) {
-      bugsnag.notify(err)
+      bugsnag.notify(err);
     }
   } catch (err) {
-    bugsnag.notify(err)
+    bugsnag.notify(err);
   }
-  return testResults
-}
+  return testResults;
+};
